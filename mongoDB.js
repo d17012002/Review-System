661018c23a877c216f616cb57adfd55e0274584a
Anurag Kumar <2002anuragksingh@gmail.com>
2022-09-07 15:49:15 +0530

    initial commit

---
+++ mongoDB.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://webconnect:webconnect123@cluster0.tnchb.mongodb.net/firefoxDB");
+
+const firefoxSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name is compulsory"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is compulsory"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is compulsory"],
+  },
+});
+
+const FirefoxUser = mongoose.model("FirefoxUser", firefoxSchema);
+
+const blacklistSchema = new mongoose.Schema({
+  faculty: {
+    type: String,
+    required: [true, "name is compulsory"]
+  },
+  reason: [String]
+});
+
+const Blacklist = mongoose.model("Blacklist", blacklistSchema);
+
+const nameListSchema = new mongoose.Schema({
+  Name: String
+})
+
+const Namelist = mongoose.model("Namelist", nameListSchema);
+
+module.exports.Anurag = new FirefoxUser({
+  name: "Anurag Kumar",
+  email: "anuragkumar2020@vitbhopal.ac.in",
+  password: "anurag123",
+});
+
+module.exports.FirefoxUser = FirefoxUser;
+module.exports.Blacklist = Blacklist;
+module.exports.Namelist = Namelist;
