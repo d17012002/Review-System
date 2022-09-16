74acbc066539ea3a335521cffd5788a94f9bd2f0
Anurag Kumar <2002anuragksingh@gmail.com>
2022-09-16 13:59:12 +0530

    reveal password

---
+++ public/javaScript/revealPass.js
@@ -0,0 +1,10 @@
+
+// make password visible
+function revealPass() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
