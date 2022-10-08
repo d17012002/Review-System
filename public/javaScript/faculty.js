b0cf66175c447219b7a9758362839af5a98dcc2d
unknown <lokeshrathoreoff@gmail.com>
2022-10-08 18:31:59 +0530

    added search box to /faculty

---
+++ public/javaScript/faculty.js
@@ -0,0 +1,34 @@
+// const input = document.querySelector(".input");
+// const accordion = document.querySelector(".accordion");
+// const panel = document.querySelector(".panel");
+
+// input.addEventListener("keyup", function(e) {
+//   // filterList(e.target.value);
+//   const o = e.target.value;
+//   accordion.forEach(listitem => {
+//     if(accordion.innerHTML.includes(o)){
+//       alert("true");
+//     }else{
+//       alert("false");
+//     }
+//   });
+// });
+
+
+// const filterList = o => {
+//   o = o.toLowerCase();
+//   accordion.forEach(listitem => {
+//     if(accordion.innerHTML.includes(o)){
+//       alert("true");
+//     }else{
+//       alert("false");
+//     }
+//     // if (label.indexOf(o) != -1) {
+//     //   listitem.style.display = "block";
+//     //   panel.style.display="block";
+//     // } else {
+//     //   listitem.style.display = "none";
+//     //   panel.style.display="none";
+//     // }
+//   });
+// };
