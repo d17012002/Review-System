a4c40b00d1f56de8c262aef0db0b7e1b49c11113
Anurag Kumar <2002anuragksingh@gmail.com>
2022-12-01 12:54:31 +0530

    faculty details search bar

--- public/javaScript/faculty.js
+++ public/javaScript/faculty.js
@@ -1,31 +1,30 @@
-var acc = document.getElementsByClassName("accordion");
-var pan = document.getElementsByClassName("panel");
+var acc = document.getElementsByClassName('accordion');
+var pan = document.getElementsByClassName('panel');
 
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + 'px';
     }
   });
 }
 
-const input = document.querySelector(".input");
-input.addEventListener("keyup", function (e) {
-  // alert("vbnm");
+const input = document.querySelector('.input');
+input.addEventListener('keyup', function (e) {
   var text = e.target.value.toLowerCase();
   for (i = 0; i < acc.length; i++) {
     if (acc[i].innerHTML.toLowerCase().includes(text)) {
-      acc[i].style.display = "block";
-      pan[i].style.display = "block";
+      acc[i].style.display = 'block';
+      pan[i].style.display = 'block';
     } else {
-      acc[i].style.display = "none";
-      pan[i].style.display = "none";
+      acc[i].style.display = 'none';
+      pan[i].style.display = 'none';
     }
   }
 });
---
+++ public/javaScript/facultyDetails.js
@@ -0,0 +1,24 @@
+var names = document.getElementsByClassName('faculty-name-row');
+var empIds = document.getElementsByClassName('faculty-empid-row');
+var faculty_row = document.getElementsByClassName('faculty-row');
+
+const input = document.querySelector('.input');
+input.addEventListener('keyup', function (e) {
+  var text = e.target.value.toLowerCase();
+  console.log(text);
+
+  for (i = 0; i < names.length; i++) {
+    if (
+      names[i].innerHTML.toLowerCase().includes(text) ||
+      empIds[i].innerHTML.toLowerCase().includes(text)
+    ) {
+      faculty_row[i].style.opacity = 100;
+    } else {
+      faculty_row[i].style.display = 'none';
+    }
+  }
+
+  if (text === '') {
+    window.location.reload();
+  }
+});
